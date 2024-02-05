1ab7767916a0f94252f4b89fcaee06ff38e98619
Aleksandar Peev <149875746+aldpeev@users.noreply.github.com>
2024-02-06 00:02:56 +0200

    add files

---
+++ L02_Arrays_Exercise/P02PrintEveryNthElement.js
@@ -0,0 +1,12 @@
+function solve(arr, step){
+   let resultArr = [];
+
+   for (let i = 0; i < arr.length; i+= step) {
+    resultArr.push(arr[i]);
+   }
+
+   return resultArr;
+}
+
+
+console.log(solve(['5', '20', '31', '4', '20'], 2));
